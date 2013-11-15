65fa1b8372659d94db1fadf3f3d0a938db94cfc3
Administrator <Administrator@OASrv>
2013-11-15 18:23:21 +0800

    0003

--- arch/src/main/java/deep/repo/PersonRepository.java
+++ arch/src/main/java/deep/repo/PersonRepository.java
@@ -14,8 +14,35 @@ package deep.repo;
  *
  */
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import deep.model.*;
-public interface PersonRepository extends JpaRepository<Person,Long> {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import deep.model.*;
+/**
+ * Specifies methods used ot obtain and modify person related information
+ * which is stored in the database.
+ * @author dev
+ *
+ */
+public interface PersonRepository extends JpaRepository<Person,Long> {
+	/**
+	 * Finds persons by using the last name as a search criteria.
+	 * @param lastName
+	 * @return	A list of persons which last name is an exact match with the given last name.
+	 * 			If no persons is found, this method returns an empty list.
+	 * 注意：
+	 * 1：方法的参数个数必须和@Query里面需要的参数个数一致
+	 * 2：如果是like，后面的参数需要前面或者后面加“%”，比如下面都对：
+	 * @Query("select o from UserModel o where o.name like ?1%")
+	 * public List<UserModel> findByUuidOrAge(String name);
+	 * @Query("select o from UserModel o where o.name like %?1")
+	 * public List<UserModel> findByUuidOrAge(String name);
+	 * @Query("select o from UserModel o where o.name like %?1%")
+	 * public List<UserModel> findByUuidOrAge(String name);
+	 */
+	@Query("SELECT p FROM Person p WHERE LOWER(p.lastName) = LOWER(:lastName)")
+	public List<Person> find(@Param("lastName") String lastName);
 }
--- arch/src/main/java/deep/svc/PersonService.java
+++ arch/src/main/java/deep/svc/PersonService.java
@@ -2,7 +2,9 @@ package deep.svc;
 import java.util.List;
 
 
+
 import deep.dto.PersonDTO;
+import deep.dto.SearchDTO;
 /**
  * Declares method used to obtain and modify person information.
  * 
@@ -44,5 +46,13 @@ public interface PersonService {
 	 * @throws	PersonNotFoundException if no person is found with given id.
 	 */
 	public Person update(PersonDTO updated);//throws PersonNotFoundException;
+	/**
+	 * Searchs persons by using the search criteria given as parameter.
+	 * 
+	 * @param searchCriteria
+	 * @return	A list of persons matching with the search criteria. If no person is found,
+	 * 			this method returns an empty list.
+	 */
+	public List<Person> search(SearchDTO searchCriteria);
 	
 }
