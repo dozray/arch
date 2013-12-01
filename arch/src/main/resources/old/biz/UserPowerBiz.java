e24986f74cbe6984cbe218194326f96ce18078c3
doz <dozray@gmail.com>
2013-12-01 18:42:02 +0800

    a

--- arch/src/main/resources/old/biz/UserPowerBiz.java
+++
@@ -1,9 +0,0 @@
-package biz;
-
-import java.util.List;
-
-import entity.Userpower;
-
-public interface UserPowerBiz {
-	List<Userpower> getPower();
-}
--- arch/src/main/resources/old/dao/UserPowerDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import java.util.List;
-
-import entity.Userpower;
-
-public interface UserPowerDao {
-	List<Userpower> getPower();
-}
